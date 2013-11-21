deb40a56e247610909255ad68551f8c5b6e104f3
Garrett Thornburg <film42@gmail.com>
2013-11-20 21:21:45 -0700

    added icon for saving to ios home screen

--- src/nataliedee/views/layout.clj
+++ src/nataliedee/views/layout.clj
@@ -6,6 +6,8 @@
     [:head
      [:title "Natalie Dee Mobile"]
      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+     [:link {:rel "apple-touch-icon" :href "/img/ios-icon.png"}]
+
      (include-css "/css/screen.css")]
     [:body
       [:header.navbar.navbar-inverse.navbar-fixed-top.bs-docs-nav
